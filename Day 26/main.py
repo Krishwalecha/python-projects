9c66b3151a5ad4c16d0bd2290c7ba3d9712c686f
Mxlfunction23 <krishwalecha098@gmail.com>
2025-06-19 15:34:16 +0530

    feat(phonetic_alphabets): add error handling for invalid user input

--- Day 26/main.py
+++ Day 26/main.py
@@ -3,7 +3,13 @@ import pandas
 data = pandas.read_csv(r'Day 26\nato_phonetic_alphabet.csv')
 nato_dict = {row.letter : row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter a word: ").upper()
-nato_list = [nato_dict[letter] for letter in user_input]
+while True:
+    user_input = input("Enter a word: ").upper().replace(" ", "")
+    try:
+        nato_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Invalid input: Please enter only alphabetic characters (A-Z). Try again.")
+    else:
+        break
 
 print(nato_list)
